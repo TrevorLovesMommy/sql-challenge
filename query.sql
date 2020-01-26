5d9130e8189dbc4db0fd27e2fa0d22e8d8ab3ac1
TrevorLovesMommy <melissa1lin@gmail.com>
2020-01-26 15:12:14 -0800

    updating homework

--- query.sql
+++ query.sql
@@ -45,7 +45,7 @@ WHERE d.dept_name = 'Sales' OR d.dept_name = 'Development';
 
 
 --#8 In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
-SELECT last_name, COUNT(last_name) as "Num Last Name"
+SELECT last_name, COUNT(last_name) as "num_last_name"
 FROM employees
 GROUP BY last_name;
 
